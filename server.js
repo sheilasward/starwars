66366c95ed324b64032b968ace233cbad0b6cc01
sheilasward <ssward1@gtcc.edu>
2018-08-31 19:15:20 -0400

    fixed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");  // Internal package - don't have to install
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 8081;
+var PORT = process.env.PORT || 8081;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
